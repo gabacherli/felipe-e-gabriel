eabf3c9afb9405be3e55c5b918727a2b344dd6cb
31432123 <31432123@088000-P33S105.macklabs.br>
2014-08-22 08:32:58 -0300

    testando o commit no dia seguinte para ver se a pasta está sincronizada com o git, etc

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 echo "teste 121321421";
  //testando a escrita do meu nome (só pra fazer o segundo commit //
- echo "Gabriel";
+ echo " - Gabriel";
+ //testando commit no dia seguinte, checando se a pasta ainda sicroniza com o github, etc.//
+ echo " - commit do dia seguinte";
 ?>

10545508d73fce68eab1d68495613cb6fd57f966
31432123 <31432123@088000-P33S105.macklabs.br>
2014-09-18 11:38:31 -0300

    alterações ideais no código - Gabriel Abacherli Coelho

--- operacoes.php
+++ operacoes.php
@@ -24,25 +24,6 @@
                 $z = $x / $y;
                 return $z;
             }
-            
-            $n1 = 5;
-            $n2 = 10;
-    
-            $resultado = soma($n1, $n2);
-            echo "Resultado da soma: ".$resultado;            
-            echo "<br><br>";
-            
-            $resultado = subtracao($n1, $n2);
-            echo "Resultado da subtração: ".$resultado;           
-            echo "<br><br>";
-           
-            $resultado = multiplicacao($n1, $n2);
-            echo "Resultado da multiplicação: ".$resultado;
-            echo "<br><br>";
-            
-            $resultado = divisao($n1, $n2);
-            echo "Resultado da divisão: ".$resultado;       
-            echo "<br><br>";
             ?>
     </body>
 </html>

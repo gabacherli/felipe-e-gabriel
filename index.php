efac6dce463b0159905363cad65284d8215e0b2d
31432123 <31432123@088000-P33S105.macklabs.br>
2014-09-18 11:28:33 -0300

    testando novo commit

--- index.php
+++ index.php
@@ -6,4 +6,6 @@ echo "teste 121321421";
  echo " - commit do dia seguinte";
  //testando commit no dia 28/08/2014
  echo " - commit do dia 28/08/14"
+ //testando commit no dia 18/09/2014
+ echo " - commit do dia 18/09/14"
 ?>

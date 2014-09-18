afe8cbe26ddebbff1846dc35b649ba9eaa0ad1bb
31432123 <31432123@088000-P33S105.macklabs.br>
2014-09-18 11:38:38 -0300

    alterações ideais no código - Gabriel Abacherli Coelho

--- teste.php
+++ teste.php
@@ -2,7 +2,24 @@
 <html>
     <body>
     <?php 
-        include "operacoes.php"; 
+            $x = 5;
+            $y = 10;
+    
+            $resultado = soma($x, $y);
+            echo "Resultado da soma: ".$resultado;            
+            echo "<br><br>";
+            
+            $resultado = subtracao($x, $y);
+            echo "Resultado da subtração: ".$resultado;           
+            echo "<br><br>";
+           
+            $resultado = multiplicacao($x, $y);
+            echo "Resultado da multiplicaçãoo: ".$resultado;
+            echo "<br><br>";
+            
+            $resultado = divisao($x, $y);
+            echo "Resultado da divisão: ".$resultado;       
+            echo "<br><br>";
     ?>
     </body>
 </html>
